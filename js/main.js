a47af3febacc946e9a183ef09ac2aaf75f0627b0
Renard Bergson <renardrock@gmail.com>
2022-09-30 20:50:26 -0300

    build: :construction: criando a estrutura de objeto base em javascript
    criando o objeto main e inserindo comentarios para poder consultar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-// code here
+const Main = {
+
+    init: function () { // this property will call the other ones
+        this.cacheSelectors() // we use "this" to say that an element is inside the principal element
+        this.bindEvents()  
+    },
+
+    cacheSelectors: function () { // this property will select the html elements 
+        this.checkButtons = document.querySelectorAll('.check') // we use "this"to make the element usable by the other functions
+    },
+
+    bindEvents: function () { // this property will call (connect) the events
+        this.checkButtons.forEach((input) => {input.onclick = this.events.checkButtonClick})
+    },
+
+    events: { // inside this property we will have the events
+        checkButtonClick: () => {alert('testing!')}
+    }
+}
+
+Main.init() // this part of the code will call the init property
