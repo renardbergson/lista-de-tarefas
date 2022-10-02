afbf2815f2905e0dffcffb87f56714ea4b179e5f
Renard Bergson <renardrock@gmail.com>
2022-10-02 20:41:16 -0300

    feat(criando funcao para inserir tarefas): :sparkles: criando funcao inserir tarefa, diminuindo duracao das animacoes

--- js/main.js
+++ js/main.js
@@ -7,18 +7,50 @@ const Main = {
 
     cacheSelectors: function () { // this property will select the html elements 
         this.$checkButtons = document.querySelectorAll('.check') // we use "this"to make the element usable by the other functions
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
     },
 
     bindEvents: function () { // this property will call (connect) the events
         this.$checkButtons.forEach((input) => {input.onclick = this.events.checkButtonClick})
+        this.$inputTask.onkeypress = this.events.inputTask_keypress.bind(this)
     },
 
     events: { // inside this property we will have the events
         checkButtonClick: (e) => {
             const $task = e.target.nextElementSibling
             $task.classList.toggle('taskDone')
+        },
+
+        inputTask_keypress: function (e) {
+            console.log(this)
+
+            const key = e.key
+            const value = e.target.value
+
+            if (key === 'Enter') {
+                this.$list.innerHTML += `
+                <li>
+                    <input type="checkbox" class="check">
+                    <!-- <div class="check"></div> -->
+                    <label class="task">
+                        ${value}
+                    </label>
+                    <button class="remove"></button>
+                </li>
+                `
+                e.target.value = ''
+            }
         }
     }
 }
 
 Main.init() // this part of the code will call the init property
+
+/* 
+1 - inside an event, "this" will always refers to the target element, to solve this, we use the BIND method, that will rebind the 
+correct one to our event
+
+2 - we can't use BIND to change the value of "this" inside an arrow function, instead we must use a normal function !!
+*/
+
